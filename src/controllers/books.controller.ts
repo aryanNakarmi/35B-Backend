1f79cba8d054438128b3f79486293e0f80e4afbf
Aryan <aaryan.nakarmi111@gmail.com>
2025-11-20 08:16:45 +0545

    commit

--- src/controllers/books.controller.ts
+++ src/controllers/books.controller.ts
@@ -1,11 +1,19 @@
 import {Request, Response} from 'express';
 
-export class BookController{
-    getBooks = (req: Request, res: Response)=>{
-        const books = [
+export type Book = {
+    id: string,
+    title: string,
+    date?: string
+};
+
+export const books = [
         {id: "B-1", title: "1984"},
         {id: "B-2", title: "To Kill a Mocking Bird",date: "2025-12-10"}
     ];
+
+export class BookController{
+    getBooks = (req: Request, res: Response)=>{
+       
     res.status(200).json(books);
 
     };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Application, Request, Response} from 'express';
 import bodyParser from 'body-parser';
-
+import bookRoutes from "./routes/book.routes"
 const app: Application = express();
 const PORT: number = 3000;
 
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.get('/', (req:Request, res:Response) =>{
     res.send('Hello, World!');
 });
-app.get("/api/books");
+app.get("/api/books",bookRoutes);
 
 app.listen(PORT,() => {
     console.log(`Server :http://localhost:${PORT}`);
