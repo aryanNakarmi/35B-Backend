a1631fb0ce5a7aa57cddb1bcb235c3d67f9800d5
Aryan <aaryan.nakarmi111@gmail.com>
2025-11-20 08:18:15 +0545

    commit

--- src/controllers/books.controller.ts
+++ src/controllers/books.controller.ts
@@ -6,15 +6,17 @@ export type Book = {
     date?: string
 };
 
-export const books = [
+export const book:Book[] = [
         {id: "B-1", title: "1984"},
         {id: "B-2", title: "To Kill a Mocking Bird",date: "2025-12-10"}
     ];
 
 export class BookController{
+    createBook = (req:Request, res: Response)=>{ };
     getBooks = (req: Request, res: Response)=>{
+        const return_book: Book[] = book;
        
-    res.status(200).json(books);
+    res.status(200).json(book);
 
     };
 }
