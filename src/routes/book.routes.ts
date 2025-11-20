e215459fbc29ef888a5c5da9829ca0f64d9b55b3
Aryan <aaryan.nakarmi111@gmail.com>
2025-11-20 08:11:27 +0545

    commit

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,9 +1,9 @@
 import {Router, Request, Response } from "express";
+import { BookController } from "../controllers/books.controller";
 
 
 const bookController = new BookController();
 const router: Router = Router();
-
 
 router.get('/',bookController.getBooks);
 export default router;
