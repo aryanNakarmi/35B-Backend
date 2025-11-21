0d1c1628753a9da9914000c399fe98f40401c12e
Aryan <aaryan.nakarmi111@gmail.com>
2025-11-21 09:57:11 +0545

    commit

---
+++ src/services/book.service.ts
@@ -0,0 +1,31 @@
+import { title } from "process";
+import { BookRepository, IBookRepository } from "../repositories/book.repositories";
+import { Book } from "../types/book.type";
+import { CreateBookDTO } from "../dtos/book.dto";
+
+let bookRepository: IBookRepository = new BookRepository();
+
+export class BookService{
+    getBooks = (): Book[] =>{
+        //business login/ transformation
+        let transformedBooks =
+        bookRepository
+        .getAllBooks()
+        .map(bk=>{
+            return{
+                ...bk,
+                title: bk.title.toUpperCase()
+            }
+        })
+        return transformedBooks;
+    }
+    createBook = (bookData:CreateBookDTO): Book => {
+        const newBook: Book = {...bookData};
+        //same as (id:bookData.id, title: bookData.id);
+        let existingBook = bookRepository.getBookById(newBook.id);
+        if(existingBook){
+            throw new Error("Book ID already exists");
+        }
+        return bookRepository.createBook(newBook);
+    }
+}
