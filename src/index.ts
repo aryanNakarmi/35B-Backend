40a4140d652669143037d065e68941c80f20ae45
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-22 09:41:06 +0545

    env

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import express, { Application, Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import bookRoutes from "./routes/book.route";
+import dotenv from 'dotenv';
+
+dotenv.config();
+//can use .env variable below this
+console.log(process.env.PORT);
 
 const app: Application = express();
 const PORT: number = 3000;
