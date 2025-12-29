7f3eac3c2006a562e3a378bab5a557b72d04ee79
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-29 09:22:12 +0545

    env file Json Token

---
+++ src/controllers/admin/user.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { AdminUserService } from "../../services/admin/user.service";
+
+let adminUserService = new AdminUserService();
+export class AdminUserController{
+    async createUser(req: Request, res: Response){
+        //can be same as AuthController.register
+    }
+    async getOneUser(req: Request, res: Response){
+        try{
+            const userId =req.params.id;
+            const user = await adminUserService.getOneUser(userId);
+            return res.status(200).json({success:true,data: user});
+        }catch(error: Error|any){
+            return res.status(error.statusCode ?? 500).json(
+                {success: false, message:error.message || "Internal Server Error"}
+            );
+        }
+    }
+}
