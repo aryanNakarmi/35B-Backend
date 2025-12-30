145bb031f98f19248e7a222ef97bca0428b7e706
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-30 11:27:30 +0545

    middleware

---
+++ src/middlewares/autharized.middleware.ts
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config";
+import { UserRepository } from "../repositories/user.repositories";
+import { NextFunction, Request, Response } from "express";
+
+
+const userRepository = new UserRepository();
+
+export const middlewareSimple = 
+    async (req: Request, res: Response, next: NextFunction) => {
+    if(req.headers && req.headers.authorization){
+        return next();
+    }
+    return res.status(401).json({ success: false,message: "Unauthorized" });
+}
