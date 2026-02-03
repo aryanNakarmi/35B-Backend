2699145fb50ad5c33f4e046221f4a5cfc82f9245
Aryan <aaryan.nakarmi111@gmail.com>
2026-02-03 12:32:52 +0545

    set up

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,6 @@
-import express, { Application, Request, Response} from 'express';
-import bodyParser from 'body-parser';
-
-import { connectDatabase } from './database/mongodb';
-import { PORT } from './config';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import path from 'path';
-
-dotenv.config();
-//can use .env variable below this
-console.log(process.env.PORT);
-
-const app: Application = express();
-
-app.use('/uploads',express.static(path.join(__dirname,'../uploads')));
-
-let corsOption = {
-    origin: ["http://localhost:3000", "http://localhost:3005"],
-    // which domain can access your backend server
-    // add frontend domain in origin
-}
-//origin: "*", //allow all domain to access your backend serveer
-app.use(cors(corsOption));  //implementt cors middleware
-
-
-//const PORT: number = 3000;
-
-app.use(bodyParser.json());
-
-import bookRoutes from "./routes/book.route";
-import authRoutes from "./routes/auth.route";
-import authUserRouter from "./routes/admin/user.route";
-import adminBlogRoutes from "./routes/admin/blog.route";
-import blogRoutes from "./routes/blog.route";
-
-
-app.use("/api/books",bookRoutes);
-app.use("/api/auth",authRoutes);
-app.use('/api/admin/users',authUserRouter);
-app.use('/api/admin/blogs',adminBlogRoutes);
-app.use('/api/blogs',blogRoutes);
-
-app.get('/', (req:Request, res:Response) =>{
-    res.send('Hello, World!');
-});
+import app from "./app";
+import { PORT } from "./config";
+import { connectDatabase } from "./database/mongodb";
 
 async function startServer() {
  await connectDatabase();
